4c7394ad0531283436dec1ea19c28fa63a9655c9
pwbhs <pwbhshshs@gmail.com>
2023-03-23 19:37:52 -0300

    Firebase integration

--- app/src/main/java/com/example/project13application/MainActivity.kt
+++ app/src/main/java/com/example/project13application/MainActivity.kt
@@ -8,13 +8,29 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.project13application.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var database: FirebaseDatabase
+    private lateinit var reference: DatabaseReference
+
+    // firebase init
+    private fun firebaseInit(){
+        database= Firebase.database
+        reference = database.getReference("test")
+    }
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sendToFirebase()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -31,5 +47,17 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    // test firebase integration
+    private fun sendToFirebase() {
+        firebaseInit()
+
+        val message = hashMapOf(
+            "message" to "This is for testing"
+        )
+
+        reference.setValue(message)
+
     }
 }
