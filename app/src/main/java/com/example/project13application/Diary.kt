e38d18d44b2623efd9664bad7b0329fe6f590c6b
pwbhs <pwbhshshs@gmail.com>
2023-03-28 19:56:36 -0300

    Setting a test form for database structure

--- app/src/main/java/com/example/project13application/Diary.kt
+++ app/src/main/java/com/example/project13application/Diary.kt
@@ -1,9 +1,24 @@
 package com.example.project13application
 
+import com.google.firebase.database.Exclude
+import kotlinx.datetime.LocalDate
+
 data class Diary(
 
     val id: String,
-    val date: String,
+    val dateString: String,
     val content: String
 
-)
+){
+    // get date data from Firebase an convert to localDate type
+    @get:Exclude
+    val date: LocalDate
+        get() = LocalDate.parse(dateString)
+
+    // the date will be set as localDate type, but store it to Firebase as String
+    companion object {
+        fun create(id: String, date: LocalDate, content: String): Diary {
+            return Diary(id, date.toString(), content)
+        }
+    }
+}
